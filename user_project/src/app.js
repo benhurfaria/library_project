e1d914a6be2d0d86345d7077662ec27b990baffa
Ben Hur Faria <benhurfaria3@gmail.com>
2023-02-14 16:17:24 -0300

    git ignore

--- user_project/src/app.js
+++ user_project/src/app.js
@@ -5,7 +5,6 @@ import * as userController from "./controller/userController.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.get("/usuario", userController.getGeral);
 
 
